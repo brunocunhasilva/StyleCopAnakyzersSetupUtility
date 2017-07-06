31359803761f186d26b1b44c28e9c39827b074bd
Bruno Cunha e Silva <bruno.cunhasilva@outsystems.com>
2017-07-06 11:23:53 +0100

    StyleCopAnalyzers setup utility baseline

---
+++ StyleCopAnalyzersSetupUtility/MainForm.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace StyleCopAnalyzersSetupUtility {
+    partial class StyleCopExcluderForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.CSProjFilePathTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.OpenFileButton = new System.Windows.Forms.Button();
+            this.ExecuteButton = new System.Windows.Forms.Button();
+            this.SingleProjectRadioButton = new System.Windows.Forms.RadioButton();
+            this.ProjectsInFolderRadioButton = new System.Windows.Forms.RadioButton();
+            this.SingleProjectPanel = new System.Windows.Forms.Panel();
+            this.ProjectsFolderPanel = new System.Windows.Forms.Panel();
+            this.FolderTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.OpenFolderButton = new System.Windows.Forms.Button();
+            this.ExcludeFromStyleCopCheckBox = new System.Windows.Forms.CheckBox();
+            this.SingleProjectPanel.SuspendLayout();
+            this.ProjectsFolderPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // CSProjFilePathTextBox
+            // 
+            this.CSProjFilePathTextBox.Location = new System.Drawing.Point(79, 3);
+            this.CSProjFilePathTextBox.Name = "CSProjFilePathTextBox";
+            this.CSProjFilePathTextBox.Size = new System.Drawing.Size(327, 20);
+            this.CSProjFilePathTextBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "CSProj";
+            // 
+            // OpenFileButton
+            // 
+            this.OpenFileButton.Location = new System.Drawing.Point(412, 3);
+            this.OpenFileButton.Name = "OpenFileButton";
+            this.OpenFileButton.Size = new System.Drawing.Size(31, 20);
+            this.OpenFileButton.TabIndex = 2;
+            this.OpenFileButton.Text = "...";
+            this.OpenFileButton.UseVisualStyleBackColor = true;
+            // 
+            // ExecuteButton
+            // 
+            this.ExecuteButton.Location = new System.Drawing.Point(79, 119);
+            this.ExecuteButton.Name = "ExecuteButton";
+            this.ExecuteButton.Size = new System.Drawing.Size(75, 23);
+            this.ExecuteButton.TabIndex = 3;
+            this.ExecuteButton.Text = "Execute";
+            this.ExecuteButton.UseVisualStyleBackColor = true;
+            // 
+            // SingleProjectRadioButton
+            // 
+            this.SingleProjectRadioButton.AutoSize = true;
+            this.SingleProjectRadioButton.Location = new System.Drawing.Point(200, 12);
+            this.SingleProjectRadioButton.Name = "SingleProjectRadioButton";
+            this.SingleProjectRadioButton.Size = new System.Drawing.Size(89, 17);
+            this.SingleProjectRadioButton.TabIndex = 4;
+            this.SingleProjectRadioButton.Text = "Single project";
+            this.SingleProjectRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // ProjectsInFolderRadioButton
+            // 
+            this.ProjectsInFolderRadioButton.AutoSize = true;
+            this.ProjectsInFolderRadioButton.Checked = true;
+            this.ProjectsInFolderRadioButton.Location = new System.Drawing.Point(81, 12);
+            this.ProjectsInFolderRadioButton.Name = "ProjectsInFolderRadioButton";
+            this.ProjectsInFolderRadioButton.Size = new System.Drawing.Size(103, 17);
+            this.ProjectsInFolderRadioButton.TabIndex = 5;
+            this.ProjectsInFolderRadioButton.TabStop = true;
+            this.ProjectsInFolderRadioButton.Text = "Projects in folder";
+            this.ProjectsInFolderRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // SingleProjectPanel
+            // 
+            this.SingleProjectPanel.Controls.Add(this.CSProjFilePathTextBox);
+            this.SingleProjectPanel.Controls.Add(this.label1);
+            this.SingleProjectPanel.Controls.Add(this.OpenFileButton);
+            this.SingleProjectPanel.Location = new System.Drawing.Point(0, 30);
+            this.SingleProjectPanel.Name = "SingleProjectPanel";
+            this.SingleProjectPanel.Size = new System.Drawing.Size(470, 28);
+            this.SingleProjectPanel.TabIndex = 7;
+            this.SingleProjectPanel.Visible = false;
+            // 
+            // ProjectsFolderPanel
+            // 
+            this.ProjectsFolderPanel.Controls.Add(this.FolderTextBox);
+            this.ProjectsFolderPanel.Controls.Add(this.label2);
+            this.ProjectsFolderPanel.Controls.Add(this.OpenFolderButton);
+            this.ProjectsFolderPanel.Location = new System.Drawing.Point(0, 30);
+            this.ProjectsFolderPanel.Name = "ProjectsFolderPanel";
+            this.ProjectsFolderPanel.Size = new System.Drawing.Size(470, 28);
+            this.ProjectsFolderPanel.TabIndex = 8;
+            // 
+            // FolderTextBox
+            // 
+            this.FolderTextBox.Location = new System.Drawing.Point(79, 3);
+            this.FolderTextBox.Name = "FolderTextBox";
+            this.FolderTextBox.Size = new System.Drawing.Size(327, 20);
+            this.FolderTextBox.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 6);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Folder";
+            // 
+            // OpenFolderButton
+            // 
+            this.OpenFolderButton.Location = new System.Drawing.Point(412, 3);
+            this.OpenFolderButton.Name = "OpenFolderButton";
+            this.OpenFolderButton.Size = new System.Drawing.Size(31, 20);
+            this.OpenFolderButton.TabIndex = 2;
+            this.OpenFolderButton.Text = "...";
+            this.OpenFolderButton.UseVisualStyleBackColor = true;
+            // 
+            // ExcludeFromStyleCopCheckBox
+            // 
+            this.ExcludeFromStyleCopCheckBox.AutoSize = true;
+            this.ExcludeFromStyleCopCheckBox.Location = new System.Drawing.Point(79, 69);
+            this.ExcludeFromStyleCopCheckBox.Name = "ExcludeFromStyleCopCheckBox";
+            this.ExcludeFromStyleCopCheckBox.Size = new System.Drawing.Size(315, 17);
+            this.ExcludeFromStyleCopCheckBox.TabIndex = 9;
+            this.ExcludeFromStyleCopCheckBox.Text = "Exclude all existent C# files from StyleCop Analyzers vigilance";
+            this.ExcludeFromStyleCopCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // StyleCopExcluderForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 163);
+            this.Controls.Add(this.ExcludeFromStyleCopCheckBox);
+            this.Controls.Add(this.ProjectsFolderPanel);
+            this.Controls.Add(this.SingleProjectPanel);
+            this.Controls.Add(this.ProjectsInFolderRadioButton);
+            this.Controls.Add(this.SingleProjectRadioButton);
+            this.Controls.Add(this.ExecuteButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "StyleCopExcluderForm";
+            this.Text = "StyleCop - CSProj Files excluder";
+            this.SingleProjectPanel.ResumeLayout(false);
+            this.SingleProjectPanel.PerformLayout();
+            this.ProjectsFolderPanel.ResumeLayout(false);
+            this.ProjectsFolderPanel.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox CSProjFilePathTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button OpenFileButton;
+        private System.Windows.Forms.Button ExecuteButton;
+        private System.Windows.Forms.RadioButton SingleProjectRadioButton;
+        private System.Windows.Forms.RadioButton ProjectsInFolderRadioButton;
+        private System.Windows.Forms.Panel SingleProjectPanel;
+        private System.Windows.Forms.Panel ProjectsFolderPanel;
+        private System.Windows.Forms.TextBox FolderTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button OpenFolderButton;
+        private System.Windows.Forms.CheckBox ExcludeFromStyleCopCheckBox;
+    }
+}
+
---
+++ StyleCopAnalyzersSetupUtility/MainForm.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace StyleCopAnalyzersSetupUtility {
+    public partial class StyleCopExcluderForm : Form {
+        private const string CSharpProjectsNamespace = "http://schemas.microsoft.com/developer/msbuild/2003";
+
+        public StyleCopExcluderForm() {
+            InitializeComponent();
+
+            ExecuteButton.Enabled = false;
+            CSProjFilePathTextBox.TextChanged += ExclusionsTextBoxs_TextChanged;
+            FolderTextBox.TextChanged += ExclusionsTextBoxs_TextChanged;
+            ProjectsInFolderRadioButton.CheckedChanged += ProjectsInFolderRadioButton_CheckedChanged;
+            OpenFileButton.Click += OpenFileButton_Click;
+            OpenFolderButton.Click += OpenFolderButton_Click;
+            ExecuteButton.Click += ExecuteButton_Click;
+            ExcludeFromStyleCopCheckBox.CheckedChanged += OptionsCheckBoxs_CheckedChanged;
+        }
+
+        private void OptionsCheckBoxs_CheckedChanged(object sender, EventArgs e) {
+            RefreshExecuteButtonState();
+        }
+
+        private void ProjectsInFolderRadioButton_CheckedChanged(object sender, EventArgs e) {
+            SingleProjectPanel.Visible = !ProjectsInFolderRadioButton.Checked;
+            ProjectsFolderPanel.Visible = ProjectsInFolderRadioButton.Checked;
+            RefreshExecuteButtonState();
+        }
+
+        private void ExecuteButton_Click(object sender, EventArgs e) {
+            Execute();
+        }
+
+        private void ExclusionsTextBoxs_TextChanged(object sender, EventArgs e) {
+            RefreshExecuteButtonState();
+        }
+
+        private void OpenFileButton_Click(object sender, EventArgs e) {
+            OpenFile();
+        }
+
+        private void OpenFolderButton_Click(object sender, EventArgs e) {
+            OpenFolder();
+        }
+
+        private void RefreshExecuteButtonState() {
+            bool enabled = false;
+
+            if (SingleProjectRadioButton.Checked) {
+                enabled = !string.IsNullOrWhiteSpace(CSProjFilePathTextBox.Text) && File.Exists(CSProjFilePathTextBox.Text);
+            } else {
+                enabled = !string.IsNullOrWhiteSpace(FolderTextBox.Text) && Directory.Exists(FolderTextBox.Text);
+            }
+
+            enabled = enabled && ExcludeFromStyleCopCheckBox.Checked;
+
+            ExecuteButton.Enabled = enabled;
+        }
+
+        private void Execute() {
+            if (SingleProjectRadioButton.Checked && ProcessCSProj(CSProjFilePathTextBox.Text)) {
+                MessageBox.Show("Operation succeeded!", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            } else if (ProjectsInFolderRadioButton.Checked) {
+                int succeededProjects;
+                int failedProjects;
+                var result = ProcessProjectsInFolder(FolderTextBox.Text, out succeededProjects, out failedProjects);
+                if (result) {
+                    MessageBox.Show($"Operation succeeded! {succeededProjects} projects successfully processed!", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                } else {
+                    MessageBox.Show($"Operation failed! {succeededProjects} projects successfully processed! {failedProjects} projects failed!", "", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+        }
+
+        private void OpenFile() {
+            var dialog = new OpenFileDialog();
+            dialog.Filter = "C# Project file | *.csproj";
+            if (dialog.ShowDialog() == DialogResult.OK) {
+                CSProjFilePathTextBox.Text = dialog.FileName;
+            }
+        }
+
+        private void OpenFolder() {
+            var dialog = new FolderBrowserDialog();
+            if (dialog.ShowDialog() == DialogResult.OK) {
+                FolderTextBox.Text = dialog.SelectedPath;
+            }
+        }
+
+        private bool ProcessProjectsInFolder(string folderPath, out int succeededProjects, out int failedProjects) {
+            succeededProjects = 0;
+            failedProjects = 0;
+
+            var files = Directory.GetFiles(folderPath, "*.csproj", SearchOption.AllDirectories);
+            foreach (var item in files) {
+                if (ProcessCSProj(item)) {
+                    succeededProjects++;
+                } else {
+                    failedProjects++;
+                }
+            }
+
+            return succeededProjects >= 0 && failedProjects == 0;
+        }
+
+        private bool ProcessCSProj(string csprojFile) {
+            if (string.IsNullOrWhiteSpace(csprojFile) || !File.Exists(csprojFile)) {
+                MessageBox.Show("Invalid csproj location");
+                return false;
+            }
+
+            try {
+                var projectDocument = new XmlDocument();
+                using (var fileStream = File.Open(csprojFile, FileMode.Open, FileAccess.ReadWrite)) {
+                    projectDocument.Load(fileStream);
+
+                    XmlNamespaceManager ns = new XmlNamespaceManager(projectDocument.NameTable);
+                    ns.AddNamespace("x", CSharpProjectsNamespace);
+
+                    var root = projectDocument.DocumentElement;
+
+                    var json = @"{
+  // ACTION REQUIRED: This file was automatically added to your project, but it
+  // will not take effect until additional steps are taken to enable it. See the
+  // following page for additional information:
+  //
+  // https://github.com/brunocunhasilva/StyleCopAnalyzers/blob/master/documentation/EnableConfiguration.md
+  
+
+  ""settings"": {
+    ""documentationRules"": {
+                        ""companyName"": ""OutSystems""
+    },
+    ""excludedFiles"": [],
+    ""excludedFileFilters"": [ ""\\.generated\\.cs$"", ""\\.designer\\.cs$"" ]
+    }
+}";
+                    var loadSettings = new JsonLoadSettings();
+                    loadSettings.CommentHandling = CommentHandling.Load;
+                    loadSettings.LineInfoHandling = LineInfoHandling.Load;
+                    var stylecopSettings = JObject.Parse(json, loadSettings);
+
+                    if (ExcludeFromStyleCopCheckBox.Checked) {
+                        ExcludeFilesFromStyleCop(projectDocument, stylecopSettings, GetCommonsThirdPartyPath(csprojFile), ns);
+                    }
+
+                    // write JSON directly to a file
+                    using (StreamWriter file = File.CreateText(Directory.GetParent(csprojFile).FullName + @"\stylecop.json"))
+                    using (JsonTextWriter writer = new JsonTextWriter(file)) {
+                        stylecopSettings.WriteTo(writer);
+                    }
+                }
+                projectDocument.Save(csprojFile);
+
+                return true;
+            } catch (Exception ex) {
+                MessageBox.Show($"Exception has occurred{Environment.NewLine}{ex.Message}{Environment.NewLine}StackTrace: {ex.StackTrace}");
+                return false;
+            }
+        }
+
+        private void ExcludeFilesFromStyleCop(XmlDocument projectDocument, JObject stylecopSettings, string commonFilesPath, XmlNamespaceManager ns) {
+            var projectNode = projectDocument.DocumentElement;
+            var compileNodes = projectNode.SelectNodes("*[local-name()='ItemGroup']/*[local-name()='Compile']", ns);
+            
+            var excludedFilesJObj = stylecopSettings["settings"]["excludedFiles"] as JArray;
+
+            var csprojAlreadySetup = false;
+            foreach (XmlNode compileNode in compileNodes) {
+                csprojAlreadySetup = csprojAlreadySetup ? csprojAlreadySetup : compileNode.SelectNodes("*[local-name()='AdditionalFiles'][@Include='stylecop.json']", ns).Count > 0;
+                
+                var includeAttr = compileNode.Attributes.GetNamedItem("Include");
+                if (includeAttr?.Value?.EndsWith(".cs", StringComparison.InvariantCultureIgnoreCase) ?? false) {
+                    excludedFilesJObj.Add(includeAttr.Value);
+                }
+            }
+
+            if (projectNode != null && !csprojAlreadySetup) {
+                var itemGroupNode = projectDocument.CreateElement("ItemGroup", CSharpProjectsNamespace);
+
+                var compileNode = compileNodes[0];
+                // add the StyleCop.json settings file to the csproj
+                var settingsNode = CreateNewProjectDocumentNode(projectDocument, itemGroupNode, "AdditionalFiles", "Include", "stylecop.json");
+
+                // add the GlobalSuppressions file link
+                var globalSuppressionsNode = CreateNewProjectDocumentNode(projectDocument, itemGroupNode, "Compile", "Include", Path.Combine(commonFilesPath, "GlobalSuppressions.cs"));
+
+                var linkNode = projectDocument.CreateElement("Link", CSharpProjectsNamespace);
+                linkNode.InnerText = "GlobalSuppressions.cs";
+                globalSuppressionsNode.AppendChild(linkNode);
+
+                // setup the analyzers references
+                CreateNewProjectDocumentNode(projectDocument, itemGroupNode, "Analyzer", "Include", Path.Combine(commonFilesPath, "Newtonsoft.Json.dll"));
+                CreateNewProjectDocumentNode(projectDocument, itemGroupNode, "Analyzer", "Include", Path.Combine(commonFilesPath, "StyleCop.Analyzers.CodeFixes.dll"));
+                CreateNewProjectDocumentNode(projectDocument, itemGroupNode, "Analyzer", "Include", Path.Combine(commonFilesPath, "StyleCop.Analyzers.dll"));
+
+                // setup json Schema for the settings file
+                var projectExtensionsNode = projectNode.SelectSingleNode("*[local-name()='ProjectExtensions']", ns);
+                if (projectExtensionsNode == null) {
+                    projectExtensionsNode = CreateNewProjectDocumentNode(projectDocument, projectNode, "ProjectExtensions", string.Empty, string.Empty);
+                }
+                var visualStudioNode = projectExtensionsNode.SelectSingleNode("*[local-name()='VisualStudio']", ns);
+                if (visualStudioNode == null) {
+                    visualStudioNode = CreateNewProjectDocumentNode(projectDocument, projectExtensionsNode, "VisualStudio", string.Empty, string.Empty);
+                }
+
+                CreateNewProjectDocumentNode(
+                    projectDocument, 
+                    visualStudioNode, 
+                    "UserProperties", 
+                    "stylecop_1json__JSONSchema", 
+                    "https://raw.githubusercontent.com/brunocunhasilva/StyleCopAnalyzers/master/StyleCop.Analyzers/StyleCop.Analyzers/Settings/stylecop.schema.json");
+
+                projectNode.AppendChild(itemGroupNode);
+            }
+        }
+
+        private XmlElement CreateNewProjectDocumentNode(XmlDocument projectDocument, XmlNode parentNode, string nodeName, string attributeName, string attributeValue) {
+            var createdNode = projectDocument.CreateElement(nodeName, CSharpProjectsNamespace);
+            if (!string.IsNullOrEmpty(attributeName)) {
+                var includeAttr = projectDocument.CreateAttribute(attributeName);
+                includeAttr.Value = attributeValue;
+                createdNode.Attributes.Append(includeAttr);
+            }
+            
+            parentNode.AppendChild(createdNode);
+
+            return createdNode;
+        }
+
+        private string GetCommonsThirdPartyPath(string csprojFile) {
+            var info = new FileInfo(csprojFile);
+            var directory = info.Directory.ToString();
+            if (!directory.EndsWith("\\")) { directory += "\\"; }
+
+            for (var level = 0; level < 20; level++) {
+                var baseRelativePath = Enumerable.Repeat("..\\", level).Aggregate(new StringBuilder(), (sb, s) => sb.Append(s)).ToString();
+                if (Directory.Exists(
+                    Path.Combine(directory + baseRelativePath,
+                    "Commons\\ThirdParty\\StyleCopAnalyzers\\"))) {
+                    return Path.Combine(baseRelativePath,
+                    "Commons\\ThirdParty\\StyleCopAnalyzers\\");
+                }
+            }
+
+            return string.Empty;
+        }
+    }
+}
